ff0084a4581ef53d536ec3cf9575909b8cabc029
Vsevolod <mrehot@gmail.com>
2023-03-10 01:49:45 +0200

    non number input fix

--- src/main/java/menu/actions/AddItemToCartAction.java
+++ src/main/java/menu/actions/AddItemToCartAction.java
@@ -5,12 +5,15 @@ import data.Item;
 import shop.Shop;
 import user_input.UserCommunication;
 
+import java.util.InputMismatchException;
+
 public class AddItemToCartAction {
 
     private static final String PROMPT_TOPIC_ITEM = "an item you wish to order: ";
     private static final String PROMPT_TOPIC_QUANTITY = " quantity to be ordered: ";
-    private static final String MESSAGE_ITEM_ADDED = "Item added to your cart";
+    private static final String MESSAGE_ITEM_ADDED = "Item added to your cart.";
     private static final String MESSAGE_NO_SUCH_ITEM = "Error: No such item.";
+    private static final String ERROR_NOT_A_NUMBER = "Error: Quantity should be a number.";
     private static final String MESSAGE_NOT_ENOUGH_QUANTITY = "Error: Available quantity lower than ordered amount.";
 
     private final UserCommunication userCommunication = new UserCommunication();
@@ -21,12 +24,16 @@ public class AddItemToCartAction {
         if (shop.itemAvailable(userInputItem)) {
             Item item = shop.findByName(userInputItem).get();
             userCommunication.requestInput(PROMPT_TOPIC_QUANTITY);
-            Integer orderedQuantity = userCommunication.getQuantity();
-            if (orderQuantityValid(orderedQuantity, item)) {
-                cart.addItem(item, orderedQuantity, shop);
-                userCommunication.informUser(MESSAGE_ITEM_ADDED);
-            } else {
-                userCommunication.informUser(MESSAGE_NOT_ENOUGH_QUANTITY);
+            try {
+                Integer orderedQuantity = userCommunication.getQuantity();
+                if (orderQuantityValid(orderedQuantity, item)) {
+                    cart.addItem(item, orderedQuantity, shop);
+                    userCommunication.informUser(MESSAGE_ITEM_ADDED);
+                } else {
+                    userCommunication.informUser(MESSAGE_NOT_ENOUGH_QUANTITY);
+                }
+            } catch (InputMismatchException e) {
+                userCommunication.informUser(ERROR_NOT_A_NUMBER);
             }
         } else {
             userCommunication.informUser(MESSAGE_NO_SUCH_ITEM);
