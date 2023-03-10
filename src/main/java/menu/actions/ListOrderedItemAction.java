b3ea59e8f349b008edaf30c1055ecaedecaadc42
aCerenkovs <dipiusik@gmail.com>
2023-03-10 20:20:00 +0100

    DataBase and InMemoryDatabase

--- src/main/java/menu/actions/ListOrderedItemAction.java
+++ src/main/java/menu/actions/ListOrderedItemAction.java
@@ -1,17 +1,19 @@
 package menu.actions;
 
 import data.OrderedItem;
+import user_input.UserCommunication;
 
 import java.util.List;
 
 public class ListOrderedItemAction {
 
+    private final UserCommunication userCommunication = new UserCommunication();
+
     public void run(List<OrderedItem> orderedItems) {
         orderedItems.forEach(System.out::println);
         if (orderedItems.isEmpty()) {
-            /* IMHO there should not be any green text in code */
-            /* we have UserCommunication for that */
-            System.out.println("Your cart is empty");
+            String MESSAGE_CART_IS_EMPTY = "Your cart is empty";
+            userCommunication.informUser(MESSAGE_CART_IS_EMPTY);
         }
     }
 }
